f7c4bc93aba920e541d487c94178313ad90c9eec
ManoelGustavo <50558011+ManoelGustavo@users.noreply.github.com>
2019-07-16 04:11:52 -0300

    Desenvolvido model para usuario

---
+++ Model/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("usuarios")]
+    public class Usuario : Base
+    {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("login")]
+        public string Login { get; set; }
+
+        [Column("senha")]
+        public string Senha { get; set; }
+    }
+}
