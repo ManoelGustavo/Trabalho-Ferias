068ac6eed57b4c83ea91d083c89d13be3f820ad2
Matheus Donato <mano_matheusblu@hotmail.com>
2019-07-18 15:26:46 -0300

    Desenvolvido Controller para Cidades

---
+++ Views/Controllers/CidadeController.cs
@@ -0,0 +1,65 @@
+﻿using Model;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private CidadeRepository repository;
+
+        public CidadeController()
+        {
+            repository = new CidadeRepository();
+        }
+
+        [HttpGet]
+        // GET: Cidade
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ObterTodos(string busca)
+        {
+            List<Cidade> cidades = repository.ObterTodos(busca);
+            return Json(cidades, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Store(Cidade cidade)
+        {
+            cidade.RegistroAtivo = true;
+            repository.Inserir(cidade);
+            return Json(cidade);
+        }
+
+        [HttpGet]
+        [Route("apagar/{id}")]
+        public JsonResult Apagar(int id)
+        {
+            bool apagou = repository.Apagar(id);
+            return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [Route("obterpeloid/{id}")]
+        public JsonResult ObterPeloId(int id)
+        {
+            Cidade cidade = repository.ObterPeloId(id);
+            return Json(cidade, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Cidade cidade)
+        {
+            bool alterou = repository.Alterar(cidade);
+            return Json(new { status = alterou });
+        }
+    }
+}
